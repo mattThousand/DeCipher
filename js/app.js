09f359651f7d89655b9ff06e1ea1336e323459c2
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-07-22 20:45:59 -0400

    Add Tweeter route and model to app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,30 @@
 App = Ember.Application.create();
 
+App.Store = DS.Store.extend({
+  revision: 12,
+  adapter: 'DS.FixtureAdapter'
+});
+
 App.Router.map(function() {
-  // put your routes here
+  this.route('tweeters');
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
     return ['thing1', 'thing2', 'thing3'];
   }
+});
+
+App.TweetersRoute = Ember.Route.extend({
+  model: function() {
+    return App.Tweeter.find();
+  }
+});
+
+var attr = DS.attr;
+
+App.Tweeter = DS.Model.extend({
+  twitter_handle: attr('string'),
+  emotion: attr('string'),
+  polarity: attr('number')
 });
