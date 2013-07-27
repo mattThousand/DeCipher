40406b52c814347b421f10a7d51d923921322cb4
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-07-27 14:47:37 -0400

    Create applicationController.js

---
+++ app/assets/javascripts/controllers/applicationController.js
@@ -0,0 +1,8 @@
+App.ApplicationController = Ember.Controller.extend({
+    atIndex: (function() {
+                  return this.get('currentRoute') === 'index';
+                    }).property('currentRoute'),
+      atTweeters: (function() {
+                     return this.get('currentRoute') === 'tweeters';
+                       }).property('currentRoute')
+});
