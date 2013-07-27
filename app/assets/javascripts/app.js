8cc3cb8ee839356742d6843307f419f256e45e50
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-07-26 23:43:50 -0400

    Create app.js

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,11 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  // routes and stuff
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    //model stuff
+  }
+});
