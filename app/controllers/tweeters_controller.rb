62fe602d38da0b51b4f2ad29ebb89fad84766ec7
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-07-27 19:01:26 -0400

    Add index method to users controller

--- app/controllers/tweeters_controller.rb
+++ app/controllers/tweeters_controller.rb
@@ -3,6 +3,11 @@ class TweetersController < ApplicationController
 
   respond_to :json
 
+  def index
+    @tweeters = Tweeter.all
+    respond_with @tweeters
+  end
+
   def show
     @tweeter = Tweeter.find(params[:id])
     respond_with @tweeter
