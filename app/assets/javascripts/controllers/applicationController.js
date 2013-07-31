d54cdc799ac4a537950f3a91a52004b94b84c5c3
Matt Buckley <matt.d.buckley1212@gmail.com>
2013-07-31 16:24:57 -0400

    Fix indentation

--- app/assets/javascripts/controllers/applicationController.js
+++ app/assets/javascripts/controllers/applicationController.js
@@ -1,8 +1,9 @@
 App.ApplicationController = Ember.Controller.extend({
-    atIndex: (function() {
-                  return this.get('currentRoute') === 'index';
-                    }).property('currentRoute'),
-      atTweeters: (function() {
-                     return this.get('currentRoute') === 'tweeters';
-                       }).property('currentRoute')
+  atIndex: (function() {
+    return this.get('currentRoute') === 'index';
+  }).property('currentRoute'),
+  atTweeters: (function() {
+    return this.get('currentRoute') === 'tweeters';
+  }).property('currentRoute')
 });
+
--- app/assets/javascripts/routes/tweetersRoute.js
+++ app/assets/javascripts/routes/tweetersRoute.js
@@ -1,18 +1,14 @@
 App.TweetersIndexRoute = Ember.Route.extend({
   model: function() {
-          return App.Tweeter.find();
-         },
-  setupController: function(controller, model) {
-          return this.controllerFor('application').set('currentRoute', 'tweeters.index');
-         }
+    return App.Tweeter.find();
+  }
 });
 
-App.TweetersNewRoute = App.TweetersRoute.extend({
-  model: function() {
-         return App.Tweeter.createRecord();
-         },
-  setupController: function() {
-          return controller.set('content', model);          
-         }
+App.TweetersNewRoute = App.TweetersIndexRoute.extend({
+    model: function() {
+     return App.Tweeter.createRecord();
+    },
+    setupController: function(controller, model) {
+      return controller.set('content', model);
+    }
 });
-
